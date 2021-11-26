be5d168ec30a2c8fcd9a14c89a5520261c78f446
syoukera <s.akira2986@gmail.com>
2021-11-26 17:00:04 +0900

    Comment out angstrom

--- src/base/Units.cpp
+++ src/base/Units.cpp
@@ -26,7 +26,7 @@ const std::map<std::string, Units> knownUnits{
     {"m", Units(1.0, 0, 1, 0)},
     {"micron", Units(1e-6, 0, 1, 0)},
     {"angstrom", Units(1e-10, 0, 1, 0)},
-    {"Å", Units(1e-10, 0, 1, 0)},
+    // {"Å", Units(1e-10, 0, 1, 0)},
 
     // Time [T]
     {"s", Units(1.0, 0, 0, 1)},
