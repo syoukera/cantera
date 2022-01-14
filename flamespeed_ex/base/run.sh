e944cbf9684675c3fc377a97631a18b48495e40d
syoukera <s.akira2986@gmail.com>
2022-01-14 14:37:32 +0900

    [flamespeed_ex] Update run.sh; imprement argument

--- flamespeed_ex/base/run.sh
+++ flamespeed_ex/base/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-phi=0.9
-eField=1.0
+# Please use this file like bellow
+# $ sh run.sh 1.1 1.0
+
+phi=$1
+eField=$2
+
+fname=phi${phi}_eField${eField}
+
+mkdir $fname
 
 g++ flamespeed.cpp -o flamespeed -pthread -O0 -std=c++0x -lcantera
-./flamespeed $phi $eField
+./flamespeed $phi $eField > ${fname}/log_${fname}
+
+mv *.csv $fname
+mv *.xml $fname
